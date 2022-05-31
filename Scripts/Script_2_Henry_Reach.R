02e4f77d1c47eb568516c9605b62ede665505b88
bridger-bertram <105084513+bridger-bertram@users.noreply.github.com>
2022-05-31 12:54:24 -0600

    Clean up environment. (Script 2 and shiny app)

--- Scripts/Script_2_Henry_Reach.R
+++ Scripts/Script_2_Henry_Reach.R
@@ -32,48 +32,46 @@ channel_complex <- pittag_data_raw %>%
                    ifelse(SC %in% c("SRSC 1", "SRSC 2"),  "SRSC" , "Lower HRSC")))
 #Create New Data Frame with Columns tag_code,complex,total_time ---- 
 
-#Filter "channel_complex" for each complex called cc_1 & cc_2
-cc_1 <- channel_complex %>% filter(Complex == "Upper HRSC")
-cc_2 <- channel_complex %>% filter(Complex == "Lower HRSC") 
-
 #Each loop runs through every unique individual that enters into their respective complex
 #and calculates the total time spent in that complex. Data are saved in "total_time_1" & "total_time_2" 
 #for Complex 1 and Complex 2 respectively. 
 
 #loop for complex 1
-fish_1 <- unique(cc_1$tag_code)
 total_time_1 <- c()
-tag_id_1 <- c()
-for(i in fish_1) {
-track_fish_1 <- channel_complex %>% filter(tag_code == i)
-total_time_1 <- c(total_time_1, max(track_fish_1$max_det) - min(track_fish_1$min_det))
-tag_id_1 <- c(tag_id_1,i)
+for(i in unique(filter(channel_complex, Complex == "Upper HRSC")$tag_code)) {
+  
+  total_time_1 <- c(total_time_1, 
+                    max((channel_complex %>% filter(Complex == "Upper HRSC", tag_code == i))$max_det) - 
+                    min((channel_complex %>% filter(Complex == "Upper HRSC", tag_code == i))$min_det))
 }
 
 #loop for complex 2
-fish_2 <- unique(cc_2$tag_code)
 total_time_2 <- c()
-tag_id_2 <- c()
-for(j in fish_2) {
-  track_fish_2 <- cc_2 %>% filter(tag_code == j)
-  total_time_2 <- c(total_time_2, max(track_fish_2$max_det) - min(track_fish_2$min_det))
-  tag_id_2 <- c(tag_id_2,j)
+for(j in unique(filter(channel_complex, Complex == "Lower HRSC")$tag_code)) {
+  
+  total_time_2 <- c(total_time_2, 
+                    max((channel_complex %>% filter(Complex == "Lower HRSC", tag_code == j))$max_det) - 
+                    min((channel_complex %>% filter(Complex == "Lower HRSC", tag_code == j))$min_det))
+  
 }
 
+
 #Create data frame and also calculate average time spent at project site.
-plot_time <- tibble(.rows = length(c(tag_id_1,tag_id_2))) %>%
-  mutate(
-    tag_id = c(tag_id_1,tag_id_2),
-    tot_time = c(total_time_1,total_time_2),
-    complex_vec = c( c(rep("Complex 1",times = length(total_time_1))), 
-                     c(rep("Complex 2",times = length(total_time_2)))))
+
 
 #Create ggplot that shows time spent at each complex.----
 
- ggplot(plot_time, aes(x=tot_time,color=complex_vec)) + 
-   geom_histogram(bins = 10, fill="white", alpha=0.5, position="identity") +
-   geom_vline(data=plot_time, aes(xintercept=mean(tot_time)),
-              linetype="dashed") +
+ ggplot(
+
+       tibble(.rows = length(c(total_time_1,total_time_2))) %>%
+         mutate(
+         tot_time = c(total_time_1,total_time_2),
+         complex_vec = c( c(rep("Complex 1",times = length(total_time_1))), 
+                       c(rep("Complex 2",times = length(total_time_2))))),
+   
+  aes(x=tot_time,color=complex_vec)) + 
+  geom_histogram(bins = 10, fill="white", alpha=0.5, position="identity") +
+  geom_vline(aes(xintercept=mean(tot_time)), linetype="dashed") +
   xlab("Days Spent in Project Site")+ 
   ylab("Number of Individuals") 
 
--- Scripts/Shiny_App_1.1.R
+++ Scripts/Shiny_App_1.1.R
@@ -1,7 +1,7 @@
 # Authors: Bridger Bertram
 # Purpose:  Pit Tag Data visualization of Henry's Reach Project. Lemhi River, Idaho
 # Created: May 15
-# Last Modified: May 30 
+# Last Modified: May 31
 
 # Load Packages and Data ------
 library(tidyverse)
@@ -43,21 +43,28 @@ ui <- fluidPage(theme = shinytheme("yeti"),
        tabsetPanel(type = "tabs",
               
               tabPanel("Daily Dections", 
+                               dateRangeInput('daterange','Select Date Range',
+                                      start = "2022-03-25",
+                                      end   = "2022-04-05",
+                                      min = as.Date(min(pittag_data_raw$min_det)),
+                                      max = as.Date(max(pittag_data_raw$min_det))),
                                selectInput('Complex','Side Channel Complex',choices = c("Lower HRSC","Upper HRSC")),
-                               plotOutput('bar_graph')), 
+                               plotOutput('bar_graph')),
+      
               tabPanel("Duration",plotOutput('Duration_Plot'))
                 
   )
  ),
- tabPanel("SRSC",plotOutput('SRSC_bar_plot'))
+ tabPanel("SRSC",
+          dateRangeInput('daterange_2','Select Date Range',
+                         start = "2022-03-25",
+                         end   = "2022-04-05",
+                         min = as.Date(min(pittag_data_raw$min_det)),
+                         max = as.Date(max(pittag_data_raw$min_det))),
+          plotOutput('SRSC_bar_plot'))
  ),
 ),
- column(5,
-        dateRangeInput('daterange','Select Date Range', 
-                       start = "2022-03-25",
-                       end   = "2022-04-05",
-                       min = as.Date(min(pittag_data_raw$min_det)),
-                       max = as.Date(max(pittag_data_raw$min_det))),
+ column(width = 5,
         leafletOutput('SC_map')
   )
  )
@@ -96,72 +103,67 @@ server <- function(input,output,session){
 
 #Duration Plot -----  
   output$Duration_Plot <- renderPlot({
-  
-    cc_1 <- channel_complex %>% filter(Complex == "Upper HRSC")
-    cc_2 <- channel_complex %>% filter(Complex == "Lower HRSC")  
 
 #Each loop runs through every unique individual that enters into their respective complex
 #and calculates the total time spent in that complex. Data are saved in "total_time_1" & "total_time_2" 
 #for Complex 1 and Complex 2 respectively.   
     
     #loop for complex 1
-    fish_1 <- unique(cc_1$tag_code)
     total_time_1 <- c()
-    tag_id_1 <- c()
-    for(i in fish_1) {
-      track_fish_1 <- cc_1 %>% filter(tag_code == i)
-      total_time_1 <- c(total_time_1, max(track_fish_1$max_det) - min(track_fish_1$min_det))
-      tag_id_1 <- c(tag_id_1,i)
+    for(i in unique(filter(channel_complex, Complex == "Upper HRSC")$tag_code)) {
+      
+      total_time_1 <- c(total_time_1, 
+                      max((channel_complex %>% filter(Complex == "Upper HRSC", tag_code == i))$max_det) - 
+                      min((channel_complex %>% filter(Complex == "Upper HRSC", tag_code == i))$min_det))
     }
     
     #loop for complex 2
-    fish_2 <- unique(cc_2$tag_code)
     total_time_2 <- c()
-    tag_id_2 <- c()
-    for(j in fish_2) {
-      track_fish_2 <- cc_2 %>% filter(tag_code == j)
-      total_time_2 <- c(total_time_2, max(track_fish_2$max_det) - min(track_fish_2$min_det))
-      tag_id_2 <- c(tag_id_2,j)
+    for(j in unique(filter(channel_complex, Complex == "Lower HRSC")$tag_code)) {
+      
+      total_time_2 <- c(total_time_2, 
+                      max((channel_complex %>% filter(Complex == "Lower HRSC", tag_code == j))$max_det) - 
+                      min((channel_complex %>% filter(Complex == "Lower HRSC", tag_code == j))$min_det))
+      
     }
 
-# Create data frame that combines data from loops to be used for ggplot.         
-    plot_time <- tibble(.rows = length(c(tag_id_1,tag_id_2))) %>%
-      mutate(
-        tag_id = c(tag_id_1,tag_id_2),
-        tot_time = c(total_time_1,total_time_2),
-        complex_vec = c( c(rep("Complex 1",times = length(total_time_1))), 
-                         c(rep("Complex 2",times = length(total_time_2)))))
-
-#Plot the time spent by juvenile Chinook in each complex.         
-    ggplot(plot_time, aes(x=tot_time,color=complex_vec)) + 
+    ggplot(
+      
+      tibble(.rows = length(c(total_time_1,total_time_2))) %>%
+        mutate(
+          tot_time = c(total_time_1,total_time_2),
+          complex_vec = c( c(rep("Complex 1",times = length(total_time_1))), 
+                           c(rep("Complex 2",times = length(total_time_2))))),
+      
+      aes(x=tot_time,color=complex_vec)) + 
       geom_histogram(bins = 10, fill="white", alpha=0.5, position="identity") +
-      geom_vline(data=plot_time, aes(xintercept=mean(tot_time)),
-                 linetype="dashed") +
+      geom_vline(aes(xintercept=mean(tot_time)), linetype="dashed") +
       xlab("Days Spent in Project Site")+ 
       ylab("Number of Individuals") 
-  })
  
+  })   
+  
 # SRSC Daily Detections  ----  
   output$SRSC_bar_plot <- renderPlot({
     
     channel_complex_SRSC <- channel_complex %>%
       filter(node %in% c(1:3)) %>% #Filter out data from "HRSC" project
-      filter(between(as.Date(min_det),input$daterange[1],input$daterange[2])) 
+      filter(between(as.Date(min_det),input$daterange_2[1],input$daterange_2[2])) 
     
-    if (input$daterange[2]-input$daterange[1]<=10) { 
+    if (input$daterange_2[2]-input$daterange_2[1]<=10) { 
       ggplot(channel_complex_SRSC, aes(x=as.Date(min_det), fill = as.factor(SC))) +
         geom_bar(color = "black") + 
         labs(title = "Daily Detections", x="Date",
              y = "Number of detections", fill = "Side Channel" ) +
         scale_x_date(date_breaks = "1 day" , labels = date_format("%m/%d/%y"),
-                     limits = c(input$daterange[1],input$daterange[2]))
+                     limits = c(input$daterange_2[1],input$daterange_2[2]))
     }else{
       ggplot(channel_complex_SRSC, aes(x=as.Date(min_det), fill = as.factor(SC))) +
         geom_bar(color = "black") + 
         labs(title = "Daily Detections", x="Date",
              y = "Number of detections", fill = "Side Channel" ) +
         scale_x_date(date_breaks = "1 week" , labels = date_format("%m/%d/%y"),
-                     limits = c(input$daterange[1],input$daterange[2]))
+                     limits = c(input$daterange_2[1],input$daterange_2[2]))
     }
  })
   
