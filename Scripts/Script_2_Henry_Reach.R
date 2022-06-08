906b2126562152a764200583884cbabec48d45f4
bridger-bertram <105084513+bridger-bertram@users.noreply.github.com>
2022-06-08 13:01:05 -0600

    shiny to-do list updates
    take out of SRSC and "duration plot".
    toggle for ortho map.
    added input for weekly and daily detection.
    
    read in of USGS discharge data for lemhi river in "script_2".
    
    deleted "Script 1".

--- Scripts/Script_2_Henry_Reach.R
+++ Scripts/Script_2_Henry_Reach.R
@@ -4,7 +4,7 @@
 # Created: May 06 
 # Last Modified: June 6
 
-#load packages & Data / Modify Data Structure -----
+#load packages & Data / Modify Data Structures -----
 library(tidyverse)
 library(sf)
 library(mapview)
@@ -13,15 +13,29 @@ library(leafem)
 library(scales)
 library(tiff)
 library(raster)
+library(dataRetrieval)
+library(padr)
 
+#Litz Cord Locations 
 litz_locs <- read_csv("Data/Litz_Locations.csv")
+#Pit Tag Data from Litz Cords 
 pittag_data_raw <- read_csv("Data/0LL_cleaned_nov_may")
+#Orthomosaic of Project Site
 ortho <- aggregate((terra::rast('Data/ortho_reduced/Henrys_reduced.tif') %>%
                    raster::brick()), fact = 3)
                    ortho[ortho == 0] <- NA
-Lemhi_200_sf <- st_transform(st_read("Data/shapefiles/Lemhi_200_rch/Lemhi_200.shp"), '+proj=longlat +datum=WGS84')   
-                   
+#Lemhi River Basin Shape File 
+Lemhi_200_sf <- st_transform(st_read("Data/shapefiles/Lemhi_200_rch/Lemhi_200.shp"), '+proj=longlat +datum=WGS84') 
 
+#Lemhi River Discharge data from USGS (Lemhi River Nr Lemhi, ID)
+Lemhi_discharge_raw <- readNWISuv(siteNumbers = "13305000", parameterCd = "00060",startDate = "2022-03-01",endDate = "2022-05-18" )
+colnames(Lemhi_discharge_raw)[4]<-"cfs"
+Lemhi_discharge <- Lemhi_discharge_raw %>%
+                   dplyr::select(dateTime,cfs) %>%
+                   mutate(day = as.Date(dateTime, format = "%d"))%>%
+                   aggregate(cfs~day,mean)  
+
+  
 channel_complex <- pittag_data_raw %>% 
   mutate(SC = ifelse(node %in% 1:2,   "SRSC 1",
               ifelse(node ==     3,   "SRSC 2",
@@ -109,14 +123,14 @@ leaflet(litz_locs %>% mutate(Side_Channel =
                 mutate(Color = c("red","red","green","green",rep("blue",6)))) %>%
    addProviderTiles('Esri.WorldImagery') %>%
    addRasterRGB(ortho , na.color = "transparent",r = 1,g = 2, b = 3, domain = 3) %>%
-   setView(lng = -113.627, lat = 44.887, zoom = 13.45) %>%
+   setView(lng = -113.627, lat = 44.899, zoom = 15.5) %>%
    addCircles(lng = ~Longitude, lat = ~Latitude, label = ~Side_Channel,
               radius = 5,
               color =~Color, 
               labelOptions = labelOptions(noHide = TRUE, offset=c(0,0), textOnly = TRUE,
                                           textsize = "10px",
                                           style = list("color" = "white" ))) %>%
-   addLegend(labels = ~unique(complex),color = ~unique(Color))
+   leaflet::addLegend(labels = ~unique(complex),color = ~unique(Color))
  
 # Shape File Leaflet ----
 leaflet(    litz_locs %>% mutate(Side_Channel = 
@@ -136,10 +150,75 @@ addCircles(lng = ~Longitude, lat = ~Latitude, label = ~complex,
              color = 'red',
              labelOptions = labelOptions(noHide = TRUE, offset=c(0,0), textOnly = TRUE,
                                          textsize = "10px"))
+ 
+ 
+#Daily Detection Plots ----
+ 
+ ggplot( filter(channel_complex,Complex == "Lower HRSC") , aes(x=as.Date(min_det), fill = as.factor(SC))) +
+   geom_bar(color = "black") + 
+   labs(title = "Daily Detections : Lower HRSC", x="Date",
+        y = "Number of detections", fill = "Side Channel" ) +
+   scale_x_date(date_breaks = "1 day" , labels = date_format("%m/%d/%y"),
+                limits = c(as.Date("2022-01-01"),as.Date("2022-05-18")))
+ 
+#Weekly Detection Plots (facets) ----
+ 
+ ggplot( channel_complex %>% 
+           filter(Complex == "Lower HRSC") %>% 
+           mutate(min_det_week = cut.POSIXt(min_det,"week")),
+           aes(x=as.Date(min_det_week))) +
+   facet_wrap(~SC, scales = "free") + 
+   geom_bar(color = "black") + 
+   labs(title = "Weekly Detections : Lower HRSC", x="Date",
+        y = "Number of detections", fill = "Side Channel" ) +
+   scale_x_date(date_breaks = "1 week" , labels = date_format("%m/%d"),
+                limits = c(as.Date("2022-01-01"),as.Date("2022-05-18")))
 
+ ggplot( channel_complex %>% 
+           filter(Complex == "Upper HRSC") %>% 
+           mutate(min_det_week = cut.POSIXt(min_det,"week")),
+         aes(x=as.Date(min_det_week))) +
+   facet_wrap(~SC, scales = "free") + 
+   geom_bar(color = "black") + 
+   labs(title = "Weekly Detections : Lower HRSC", x="Date",
+        y = "Number of detections", fill = "Side Channel" ) +
+   scale_x_date(date_breaks = "1 week" , labels = date_format("%m/%d"),
+                limits = c(as.Date("2022-01-01"),as.Date("2022-05-018")))
 
+#Weekly Detection Plots (stacked bars) ----
 
+ ggplot( channel_complex %>% 
+           filter(Complex == "Lower HRSC") %>% 
+           mutate(min_det_week = cut.POSIXt(min_det,"week")),
+         aes(x=as.Date(min_det_week), fill = as.factor(SC))) +
+   geom_bar(color = "black") + 
+   labs(title = "Weekly Detections : Lower HRSC", x="Date",
+        y = "Number of detections", fill = "Side Channel" ) +
+   scale_x_date(date_breaks = "1 week" , labels = date_format("%m/%d"),
+                limits = c(as.Date("2022-01-01"),as.Date("2022-05-18")))
 
+# Lemhi CFS and Daily Dets ---- 
 
+ #   daily_dets <- as.data.frame(table(
+ #     pittag_data_raw %>%
+ #       mutate(day = as.Date(min_det, format = "%d")) %>% 
+ #       dplyr::select(day) %>% 
+ #       filter(between(day, as.Date("2022-03-01"),as.Date("2022-05-18")))
+ #   )) 
+ # daily_dets[1] <- as.Date(daily_dets$day) 
+ # 
+ # daily_dets_complete <- daily_dets %>% pad %>%
+ #   fill_by_value(Freq)
+ # 
+ # ggplot(daily_dets, aes( x=day , y=Freq ))+ 
+ #  geom_line() 
+ # 
+ # ggplot(Lemhi_discharge, aes(x = day, y=cfs)) + 
+ #   geom_line()
+ 
 
-
+ 
+ 
+ 
+ 
+ 
--- Scripts/Shiny_App_1.1.R
+++ Scripts/Shiny_App_1.1.R
@@ -38,185 +38,158 @@ channel_complex <- pittag_data_raw %>%
                    ifelse(SC %in% c("SRSC 1", "SRSC 2"),  "SRSC" , "Lower HRSC")))
 
 # ui ----
-ui <- fluidPage(theme = shinytheme("yeti"),
-  titlePanel("Henry's Reach Detection Data"),
+ui <- fluidPage(theme = shinytheme("spacelab"),
+  titlePanel("Henry's Reach"),
    fluidRow(
       column(7,
        tabsetPanel(
-         tabPanel("HRSC", 
-          tabsetPanel(type = "tabs",
-              tabPanel("Daily Detections", 
-                               dateRangeInput('daterange','Select Date Range',
-                                      start = "2022-03-25",
-                                      end   = "2022-04-05",
-                                      min = as.Date(min(pittag_data_raw$min_det)),
-                                      max = as.Date(max(pittag_data_raw$min_det))),
-                               selectInput('Complex','Side Channel Complex',choices = c("Lower HRSC","Upper HRSC")),
-                               plotOutput('bar_graph')),
-              tabPanel("Duration",plotOutput('Duration_Plot'))
-                   ) #tabsetPannel (within HRSC tab)
-                 ), #tabPannel "HRSC"
- tabPanel("SRSC",
-   tabsetPanel(type = "tabs", 
-      tabPanel("Daily Detections",      
-          dateRangeInput('daterange_2','Select Date Range',
-                         start = "2022-03-25",
-                         end   = "2022-04-05",
-                         min = as.Date(min(pittag_data_raw$min_det)),
-                         max = as.Date(max(pittag_data_raw$min_det))),
-          plotOutput('SRSC_bar_plot')),
-      tabPanel("Duration",plotOutput('Duration_Plot_SRSC'))
-                ), #tabsetPannel (within "SRSC" tab)
-               ), #tabPanel "SRSC"
+         tabPanel("Site Detection", 
+                       dateRangeInput('daterange','Select Date Range',
+                              start = "2022-04-20",
+                              end   = "2022-05-18",
+                              min   = as.Date(min(pittag_data_raw$min_det)),
+                              max   = as.Date(max(pittag_data_raw$min_det))),
+                        fluidRow(
+                          column(width = 6, selectInput('Complex','Side Channel Complex',choices = c("Lower HRSC","Upper HRSC"))),
+                          column(width = 6,selectInput('time_frame','Detections by...',choices = c("day","week"), selected = "week"))
+                                ),  #fluidRow (select inputs)
+                        plotOutput('bar_graph')),
+         tabPanel("Fish Movement"), #tabPanel "fish movement"
               ), #tabsetPanel (main)
              ), #column
  column(width  = 5, 
         offset = 0, 
+        radioButtons('ortho_choice', 'Orthomosaic', 
+                     choices = c('Display', 'Remove'),
+                     inline = TRUE), 
         leafletOutput('SC_map', height = "85vh"))
+   
   ), #fluidRow
 ) #fluidPage
 
 server <- function(input,output,session){
 
-#Henry's Reach Daily Detentions ------   
+#Henry's Reach  Detection ------   
   # This plot describes the daily detentions at each side channel. Data is filtered 
   # based on the date range input. The X-axis is modified with the "if" statement to
   # improve readability. 
   
   output$bar_graph <- renderPlot({
-    
-    channel_complex_daily_plot <- channel_complex %>%
-    filter(node %in% c(4:18)) %>% #Filter out data from "SRSC" project
-    filter(between(as.Date(min_det),input$daterange[1],input$daterange[2]) 
-           , Complex == input$Complex) 
+
     
     if (input$daterange[2]-input$daterange[1]<=10) {
       
-    ggplot(channel_complex_daily_plot, aes(x=as.Date(min_det), fill = as.factor(SC))) +
+    ggplot(channel_complex %>%
+           filter(between(as.Date(min_det),input$daterange[1],input$daterange[2]), 
+           Complex == input$Complex) %>% 
+           mutate(min_det_scaled = cut.POSIXt(min_det,input$time_frame)), 
+           aes(x=as.Date(min_det_scaled), fill = as.factor(SC))) +
       geom_bar(color = "black") + 
-      labs(title = "Daily Detections", x="Date",
-           y = "Number of detections", fill = "Side Channel" ) +
+      labs(x="Date", y = "Number of detections", fill = "Side Channel" ) +
       scale_x_date(date_breaks = "1 day" , labels = date_format("%m/%d/%y"),
-                   limits = c(input$daterange[1],input$daterange[2]))
-      
+                   limits = c(input$daterange[1]-2,input$daterange[2]+2)) 
+    
     }else{
-      
-    ggplot(channel_complex_daily_plot, aes(x=as.Date(min_det), fill = as.factor(SC))) +
-      geom_bar(color = "black") + 
-      labs(title = "Daily Detections", x="Date",
-            y = "Number of detections", fill = "Side Channel" ) +
+
+    ggplot(channel_complex %>%
+           filter(between(as.Date(min_det),input$daterange[1],input$daterange[2]),
+           Complex == input$Complex) %>%
+           mutate(min_det_scaled = cut.POSIXt(min_det,input$time_frame)),
+           aes(x=as.Date(min_det_scaled), fill = as.factor(SC))) +
+      geom_bar(color = "black") +
+      labs( x="Date",y = "Number of detections", fill = "Side Channel" ) +
       scale_x_date(date_breaks = "1 week" , labels = date_format("%m/%d/%y"),
-                     limits = c(input$daterange[1],input$daterange[2]))
+                     limits = c(input$daterange[1]-2,input$daterange[2]+2))
     }
   })
 
-#Duration Plot -----  
-  output$Duration_Plot <- renderPlot({
-
-#Each loop runs through every unique individual that enters into their respective complex
-#and calculates the total time spent in that complex. Data are saved in "total_time_1" & "total_time_2" 
-#for Complex 1 and Complex 2 respectively.   
-    
-    #loop for complex 1
-    total_time_1 <- c()
-    for(i in unique(filter(channel_complex, Complex == "Upper HRSC")$tag_code)) {
-      
-      total_time_1 <- c(total_time_1, 
-                      max((channel_complex %>% filter(Complex == "Upper HRSC", tag_code == i))$max_det) - 
-                      min((channel_complex %>% filter(Complex == "Upper HRSC", tag_code == i))$min_det))
-    }
-    
-    #loop for complex 2
-    total_time_2 <- c()
-    for(j in unique(filter(channel_complex, Complex == "Lower HRSC")$tag_code)) {
-      
-      total_time_2 <- c(total_time_2, 
-                      max((channel_complex %>% filter(Complex == "Lower HRSC", tag_code == j))$max_det) - 
-                      min((channel_complex %>% filter(Complex == "Lower HRSC", tag_code == j))$min_det))
-      
-    }
-
-    ggplot(
-      
-      tibble(.rows = length(c(total_time_1,total_time_2))) %>%
-      mutate(tot_time = c(total_time_1,total_time_2),
-             complex = c( c(rep("Upper HRSC",times = length(total_time_1))), 
-             c(rep("Lower HRSC",times = length(total_time_2))))),
-             aes(x=tot_time,color=complex)) + 
-      
-      geom_histogram(bins = 10, fill="white", alpha=0.5, position="identity") +
-      geom_vline(aes(xintercept=mean(tot_time)), linetype="dashed") +
-      xlab("Days Spent in Project Site")+ 
-      ylab("Number of Individuals") 
- 
-  })   
-  
-# SRSC Daily Detection  ----  
-  output$SRSC_bar_plot <- renderPlot({
-    
-    channel_complex_SRSC <- channel_complex %>%
-      filter(node %in% c(1:3)) %>% #Filter out data from "HRSC" project
-      filter(between(as.Date(min_det),input$daterange_2[1],input$daterange_2[2])) 
-    
-    if (input$daterange_2[2]-input$daterange_2[1]<=10) { 
-      ggplot(channel_complex_SRSC, aes(x=as.Date(min_det), fill = as.factor(SC))) +
-        geom_bar(color = "black") + 
-        labs(title = "Daily Detections", x="Date",
-             y = "Number of detections", fill = "Side Channel" ) +
-        scale_x_date(date_breaks = "1 day" , labels = date_format("%m/%d/%y"),
-                     limits = c(input$daterange_2[1],input$daterange_2[2]))
-    }else{
-      ggplot(channel_complex_SRSC, aes(x=as.Date(min_det), fill = as.factor(SC))) +
-        geom_bar(color = "black") + 
-        labs(title = "Daily Detections", x="Date",
-             y = "Number of detections", fill = "Side Channel" ) +
-        scale_x_date(date_breaks = "1 week" , labels = date_format("%m/%d/%y"),
-                     limits = c(input$daterange_2[1],input$daterange_2[2]))
-    }
- })
-
-# SRSC Duration Plot ----
-  output$Duration_Plot_SRSC <- renderPlot({
-    total_time_3 <- c()
-    for(k in unique(filter(channel_complex, Complex == "SRSC")$tag_code)) {
-      
-      total_time_3 <- c(total_time_3, 
-                        max((channel_complex %>% filter(Complex == "SRSC", tag_code == k))$max_det) - 
-                        min((channel_complex %>% filter(Complex == "SRSC", tag_code == k))$min_det))
-    } 
-    
-    ggplot(
-      
-      tibble(.rows = length(total_time_3)) %>%
-        mutate(tot_time = total_time_3),
-      aes(x=tot_time)) + 
-      
-      geom_histogram(bins = 10, alpha=0.5, color = "red") +
-      geom_vline(aes(xintercept=mean(tot_time)), linetype="dashed") +
-      xlab("Days Spent in Project Site") + 
-      ylab("Number of Individuals")
-  })
+# #Duration Plot -----  
+#   output$Duration_Plot <- renderPlot({
+# 
+# #Each loop runs through every unique individual that enters into their respective complex
+# #and calculates the total time spent in that complex. Data are saved in "total_time_1" & "total_time_2" 
+# #for Complex 1 and Complex 2 respectively.   
+#     
+#     #loop for complex 1
+#     total_time_1 <- c()
+#     for(i in unique(filter(channel_complex, Complex == "Upper HRSC")$tag_code)) {
+#       
+#       total_time_1 <- c(total_time_1, 
+#                       max((channel_complex %>% filter(Complex == "Upper HRSC", tag_code == i))$max_det) - 
+#                       min((channel_complex %>% filter(Complex == "Upper HRSC", tag_code == i))$min_det))
+#     }
+#     
+#     #loop for complex 2
+#     total_time_2 <- c()
+#     for(j in unique(filter(channel_complex, Complex == "Lower HRSC")$tag_code)) {
+#       
+#       total_time_2 <- c(total_time_2, 
+#                       max((channel_complex %>% filter(Complex == "Lower HRSC", tag_code == j))$max_det) - 
+#                       min((channel_complex %>% filter(Complex == "Lower HRSC", tag_code == j))$min_det))
+#       
+#     }
+# 
+#     ggplot(
+#       
+#       tibble(.rows = length(c(total_time_1,total_time_2))) %>%
+#       mutate(tot_time = c(total_time_1,total_time_2),
+#              complex = c( c(rep("Upper HRSC",times = length(total_time_1))), 
+#              c(rep("Lower HRSC",times = length(total_time_2))))),
+#              aes(x=tot_time,color=complex)) + 
+#       
+#       geom_histogram(bins = 10, fill="white", alpha=0.5, position="identity") +
+#       geom_vline(aes(xintercept=mean(tot_time)), linetype="dashed") +
+#       xlab("Days Spent in Project Site")+ 
+#       ylab("Number of Individuals") 
+#  
+#   })   
   
 # Leaflet Map of Project Site ---- 
   output$SC_map <- renderLeaflet({
+    
+    if (input$ortho_choice == 'Remove'){
+      
+      leaflet(litz_locs %>% mutate(Side_Channel = 
+                                     c("NA",  "NA"  , "NA", "HRSC 1",  "NA"   , 
+                                       "HRSC 2",  "NA"  , "HRSC 3",  "NA"   , "HRSC 4", 
+                                       "NA"   ,"HRSC 5",  "NA"   , "HRSC 6",  "NA"   , 
+                                       "NA"   ,"HRSC 7", "HRSC 8")) %>%
+                filter(Side_Channel != "NA") %>%
+                mutate(complex = c(rep("Upper HRSC",2),rep("Lower HRSC",6)))%>%
+                mutate(Color = c("green","green",rep("blue",6)))) %>%
+        addProviderTiles('Esri.WorldImagery') %>%
+        setView(lng = -113.627, lat = 44.899, zoom = 15.5) %>%
+        addCircles(lng = ~Longitude, lat = ~Latitude, label = ~Side_Channel,
+                   radius = 5,
+                   color =~Color, 
+                   labelOptions = labelOptions(noHide = TRUE, offset=c(0,0), textOnly = TRUE,
+                                               textsize = "10px",
+                                               style = list("color" = "white" ))) %>%
+        leaflet::addLegend(labels = ~unique(complex),color = ~unique(Color))
+      
+    }else{
+      
     leaflet(litz_locs %>% mutate(Side_Channel = 
-                                   c("SRSC 1",  "NA"  , "SRSC 2", "HRSC 1",  "NA"   , 
+                                   c("NA",  "NA"  , "NA", "HRSC 1",  "NA"   , 
                                      "HRSC 2",  "NA"  , "HRSC 3",  "NA"   , "HRSC 4", 
                                       "NA"   ,"HRSC 5",  "NA"   , "HRSC 6",  "NA"   , 
                                       "NA"   ,"HRSC 7", "HRSC 8")) %>%
               filter(Side_Channel != "NA") %>%
-              mutate(complex = c("SRSC","SRSC",rep("Upper HRSC",2),rep("Lower HRSC",6)))%>%
-              mutate(Color = c("red","red","green","green",rep("blue",6)))) %>%
+              mutate(complex = c(rep("Upper HRSC",2),rep("Lower HRSC",6)))%>%
+              mutate(Color = c("green","green",rep("blue",6)))) %>%
       addProviderTiles('Esri.WorldImagery') %>%
       addRasterRGB(ortho , na.color = "transparent", r = 1,  g = 2,  b = 3, domain = 3)%>%
-      setView(lng = -113.627, lat = 44.887, zoom = 14) %>%
+      setView(lng = -113.627, lat = 44.899, zoom = 15.5) %>%
       addCircles(lng = ~Longitude, lat = ~Latitude, label = ~Side_Channel,
                  radius = 5,
                  color =~Color, 
                  labelOptions = labelOptions(noHide = TRUE, offset=c(0,0), textOnly = TRUE,
                                              textsize = "10px",
                                              style = list("color" = "white" ))) %>%
-      addLegend(labels = ~unique(complex),color = ~unique(Color))
+      leaflet::addLegend(labels = ~unique(complex),color = ~unique(Color))
+    }
+
   })
+  
 }
 shinyApp(ui=ui,server=server)
